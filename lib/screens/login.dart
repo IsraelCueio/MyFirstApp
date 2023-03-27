2f82cdacc32f86176ed380aab815e4a928a27361
IsraelCueio <israelcoelhocunhaccc@gmail.com>
2023-03-27 17:34:36 -0300

    Adding navigation rail (Not functional yet)

---
+++ lib/screens/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: ,
+        title: Text(
+          'Sou lindo',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
